182427030d2663d1e33c1d1ccc5e9c0022ab7917
jhenrique09 <jhsv09@gmail.com>
2023-08-01 00:13:25 +0530

    vendor: Allow using legacy fd
    Change-Id: I84c946cc9ced312cd45e39092d89a6a9d1a0312d
    Signed-off-by: itsxrp <itsxrproms@gmail.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -47,7 +47,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_legacy_fd_fbdev
 
 SOONG_CONFIG_NAMESPACES += afterlifeNvidiaVars
 SOONG_CONFIG_afterlifeNvidiaVars += \
@@ -76,6 +77,7 @@ SOONG_CONFIG_afterlifeNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEW
 SOONG_CONFIG_afterlifeQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_afterlifeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_afterlifeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_afterlifeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
