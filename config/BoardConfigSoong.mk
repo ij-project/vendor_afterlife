a6cffad4c6a2d10ba081bf3ec0aa7f57144ba4dc
Anushek Prasal <anushekprasal@gmail.com>
2024-04-09 07:15:10 +0000

    vendor: soong: Add camera_needs_client_info
    Change-Id: I44c59fdf658a241aa20c715782b14041b19dc234
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -56,7 +56,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
     needs_camera_boottime \
-    target_camera_package_name
+    target_camera_package_name \
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += afterlifeNvidiaVars
 SOONG_CONFIG_afterlifeNvidiaVars += \
@@ -76,8 +77,8 @@ SOONG_CONFIG_afterlifeQcomVars += \
 endif
 
 # Soong bool variables
-<<<<<<< HEAD
 SOONG_CONFIG_afterlifeGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
+SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_afterlifeGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_afterlifeGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
