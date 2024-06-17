zaidkhan0997/vendor_blaze

soong: extend support for oppo camera
Change-Id: Ic24a503298587f55d0acff2381c98093c51daa87
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -59,7 +59,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_camera_package_name \
     camera_needs_client_info \
     uses_nothing_camera \
-    uses_miui_camera
+    uses_miui_camera \
+    uses_oppo_camera
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -98,6 +99,7 @@ SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_C
 SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_blazeGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
+SOONG_CONFIG_blazeGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

ij-project/vendor_afterlife

Change-Id: I8e63e6842c792d1c9cc8101a247721262c4b0c96
Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

config/BoardConfigSoong.mk
@@ -59,7 +59,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     target_camera_package_name \
     camera_needs_client_info \
     uses_nothing_camera \
-    uses_miui_camera
+    uses_miui_camera \
+    uses_oppo_camera
 
 SOONG_CONFIG_NAMESPACES += afterlifeNvidiaVars
 SOONG_CONFIG_afterlifeNvidiaVars += \
@@ -96,6 +97,7 @@ SOONG_CONFIG_afterlifeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD
 SOONG_CONFIG_afterlifeGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_afterlifeGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_afterlifeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 