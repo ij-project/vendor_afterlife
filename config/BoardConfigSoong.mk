zaidkhan0997/vendor_blaze

soong: Add TARGET_USES_NOTHING_CAMERA
Change-Id: Ib03ed29f9649750fa0f10f28615c109ded21b55a
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
Signed-off-by: aswin7469 <aswinas@pixysos.com>
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -57,7 +57,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    camera_needs_client_info
+    camera_needs_client_info \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -93,6 +94,7 @@ SOONG_CONFIG_blazeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGE
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

ij-project/vendor_afterlife

Change-Id: If29b47448782202dc0303690709073a8770cd5c9
Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

config/BoardConfigSoong.mk
@@ -57,7 +57,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    camera_needs_client_info
+    camera_needs_client_info \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += afterlifeNvidiaVars
 SOONG_CONFIG_afterlifeNvidiaVars += \
@@ -90,6 +91,7 @@ SOONG_CONFIG_afterlifeQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VI
 SOONG_CONFIG_afterlifeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_afterlifeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_afterlifeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_afterlifeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_afterlifeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)