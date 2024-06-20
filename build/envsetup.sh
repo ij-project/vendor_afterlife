47f5d0cfb9211297fcde3fc547b37b7f5469ab24
Aditya Eka Pranata <aepranata@gmail.com>
2024-06-21 03:04:44 +0700

    envsetup: Introduce AfterLife build command
    Change-Id: Ib30ebf6892283d0fff74605a6892e15eccdad645
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -53,15 +53,31 @@ function mk_timer()
     return $ret
 }
 
-function brunch()
+function goafterlife()
 {
-    breakfast $*
-    if [ $? -eq 0 ]; then
-        mka bacon
+    target=$1
+    local variant=$2
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
     else
-        echo "No such item in brunch menu. Try 'breakfast'"
-        return 1
+        if [[ "$target" =~ -(user|userdebug|eng)$ ]]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the AfterLife model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+
+            lunch afterlife_$target-$variant
+        fi
+
+        make cleaninstall
+        mka afterlife
     fi
+
     return $?
 }
 
