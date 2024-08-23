1f097d3330a087f9c214960b47fa1f6de664b2a7
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-23 13:13:45 +0530

    vendor: Add properties for mainline spoofing
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config/pixel_props.mk
+++ config/pixel_props.mk
@@ -27,3 +27,19 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
     setupwizard.theme=glif_v3_light \
     setupwizard.feature.enable_wifi_tracker=true
+
+# Mainline Spoofing
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_mainline_BRAND?=google \
+    persist.sys.pihooks_mainline_DEVICE?=caiman \
+    persist.sys.pihooks_mainline_MANUFACTURER?=Google \
+    persist.sys.pihooks_mainline_PRODUCT?=caiman \
+    ro.product.model_for_attestation?=Pixel 9 Pro \
+    ro.product.brand_for_attestation?=google \
+    ro.product.name_for_attestation?=caiman \
+    ro.product.device_for_attestation?=caiman \
+    ro.product.manufacturer_for_attestation?=Google
+    
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PIHOOKS_BUILD_FINGERPRINT="google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys" \
+    PIHOOKS_MODEL_SPOOF="Pixel 9 Pro"
